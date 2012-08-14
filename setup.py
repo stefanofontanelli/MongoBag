9168d3f37daf39d14a594b186ad9d1ed973f5e40
Stefano Fontanelli <s.fontanelli@asidev.com>
2012-08-14 18:22:10 +0200

    Add package setup files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='MongoBag',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[],
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='pymongo mongodb mongo colander',
+      author='Stefano Fontanelli',
+      author_email='s.fontanelli@asidev.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'pymongo',
+          'colander'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      tests_require=[],
+      test_suite='tests',
+      )
